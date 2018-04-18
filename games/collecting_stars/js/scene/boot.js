d67cd5c171a3915b34f1a0dd8356e00f241fdae3
thom <jose.thomazini@gmail.com>
2018-04-17 23:50:50 -0300

    removes boot scene, since load can do all the job

--- games/collecting_stars/js/scene/boot.js
+++
@@ -1,9 +0,0 @@
-let boot = new Phaser.Scene('boot');
-
-boot.preload = function() {
-    this.load.image('progress_bar', 'img/progress_bar.png');
-};
-
-boot.create = function() {
-    this.scene.launch('load');
-};
