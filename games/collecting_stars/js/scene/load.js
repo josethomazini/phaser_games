84d59dfc717552d77e80fd60aebd3ea0f72bfb97
thom <jose.thomazini@gmail.com>
2018-04-17 23:58:57 -0300

    progress bar and assets loader

--- games/collecting_stars/js/scene/load.js
+++ games/collecting_stars/js/scene/load.js
@@ -1,9 +1,45 @@
 let load = new Phaser.Scene('load');
 
 load.preload = function() {
-    make_loading_text(this);
+    this.make_loading_text();
+    this.start_progress_loader();
+    this.load_assets();
 };
 
 load.create = function() {
+    this.scene.start('stage');
+};
 
+load.load_assets = function() {
+    load_images(this);
+    load_spritesheets(this);
+    load_audios(this);
+    load_bitmap_font(this);
+}
+
+load.start_progress_loader = function() {
+    let progress = this.add.graphics();
+
+    this.load.on('progress', function(value) {
+        progress.clear();
+        progress.fillStyle(0xffffff, 1);
+        progress.fillRect(0, 270, 800 * value, 60);
+
+    });
+
+    this.load.on('complete', function () {
+        progress.destroy();
+    });
+};
+
+load.make_loading_text = function() {
+    let x = config.width / 2;
+    let y = config.height / 3;
+
+    let text = this.add.text(
+        x, y, 'Loading',
+        {font: '50px Arial', fill: '#fff'}
+    );
+
+    text.setOrigin(0, 0);
 };
