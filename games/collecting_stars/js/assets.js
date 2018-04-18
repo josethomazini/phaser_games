e9539d7e9faa8d4dc9184377785fd46c5e2d5115
thom <jose.thomazini@gmail.com>
2018-04-17 23:53:55 -0300

    template for the assets loader and generated file

---
+++ games/collecting_stars/js/assets.js
@@ -0,0 +1,14 @@
+// This file was auto-generated. Do not edit it!
+
+function load_images(obj) {
+}
+
+function load_spritesheets(obj) {
+}
+
+function load_audios(obj) {
+}
+
+function load_bitmap_font(obj) {
+	obj.load.bitmapFont('nokia16black', 'font/nokia16black.png', 'font/nokia16black.xml');
+}
---
+++ templates/assets.js
@@ -0,0 +1,11 @@
+function load_images(obj) {{{ load_images }}
+}
+
+function load_spritesheets(obj) {{{ load_spritesheets }}
+}
+
+function load_audios(obj) {{{ load_audios }}
+}
+
+function load_bitmap_font(obj) {{{ load_bitmap_font }}
+}
